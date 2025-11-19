c93efabcdd280103912734485882e4c182968888
apska <apska@mail.ru>
2025-11-19 12:58:52 +0300

    Пофиксил di

--- data/src/main/java/com/apska/data/di/DataModule.kt
+++ data/src/main/java/com/apska/data/di/DataModule.kt
@@ -1,18 +1,28 @@
 package com.apska.data.di
 
+import com.apska.data.dataSource.remote.WeatherApi
+import com.apska.data.dataSource.remote.WeatherApiService
 import com.apska.data.repository.Repository
 import com.apska.domain.repository.WeatherRepository
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class DataModule {
+class DataModule {
 
-    @Binds
-    abstract fun bindWeatherRepository(
-        repository: Repository
-    ) : WeatherRepository
+    @Provides
+    @Singleton
+    fun provideWeatherApiService() : WeatherApiService {
+        return WeatherApi.api
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(weatherApiService: WeatherApiService) : WeatherRepository {
+        return Repository(weatherApiService)
+    }
 }
